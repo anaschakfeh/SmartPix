8f83660c99eb5e1a88e653a80b345a69e820b835
AnasChakfeh <Anas.Chakfeh@gmail.com>
2013-09-29 20:24:27 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TwtrSmartPix::Application.config.secret_key_base = 'b41866d1c20b7813b648f10e14c03710a977c1aa10ba0465f42ead332560e1d7cf7261d5ab519c4c0ecae3877b1dd3ef80a6193b0cf56ec62c16d1116ef06cb8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TwtrSmartPix::Application.config.session_store :cookie_store, key: '_TwtrSmartPix_session'
